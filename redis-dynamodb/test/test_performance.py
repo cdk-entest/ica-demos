7a9a914149006c8a4985d530d88d247fd6e8aa4d
haitran <hai@entest.io>
2022-06-20 10:30:22 +0000

    ok

--- redis-dynamodb/test/test_performance.py
+++ redis-dynamodb/test/test_performance.py
@@ -1,3 +1,8 @@
+"""
+Hai Tran 20 JUNE 2022 
+Redis and DynamoDB 
+"""
+
 import os
 import time
 import redis 
@@ -12,6 +17,52 @@ HOST = os.environ["REDIS_HOSTNAME"].replace(":6379", "")
 dynamodb = boto3.client("dynamodb")
 # redis client
 r = redis.Redis(host=HOST)
+
+def fetch_restaurant_summary(restaurant_name):
+    """
+    fetch from cache and write to cache 
+    """
+    # fetch from cache
+    start = time.perf_counter()
+    restaurant = fetch_restaurant_summary_from_cache(restaurant_name)
+    end = time.perf_counter()
+    print(f'cache query latency {(end - start) * 1000:.4f}ms')
+    # hit cache and return 
+    if restaurant:
+        # print_restaurant(restaurant)
+        print("Using cached result!")
+        return (end-start)*1000
+    # mis-cache and fetch from db
+    restaurant = fetch_restaurant_summary_from_local(restaurant_name)
+    # write to cache 
+    store_restaurant_summary_in_cache(restaurant)
+    # print response 
+    print("Using uncached result!")
+    return (end-start)*1000
+
+
+def fetch_restaurant_summary_from_db(restaurant_name):
+    """
+    from directory from db 
+    """
+    start = time.perf_counter()
+    resp = dynamodb.query(
+        TableName="Restaurants",
+        IndexName="GSI1",
+        KeyConditionExpression="GSI1PK = :gsi1pk",
+        ExpressionAttributeValues={
+            ":gsi1pk": {"S": "REST#{}".format(restaurant_name)},
+        },
+        ScanIndexForward=False,
+        Limit=6,
+    )
+    end = time.perf_counter()
+    print(f'db query latency {(end - start) * 1000: .4f}ms')
+    restaurant = Restaurant(resp["Items"][0])
+    restaurant.reviews = [Review(item) for item in resp["Items"][1:]]
+    # print_restaurant(restaurant)
+    # return 
+    return (end-start)*1000
 
 
 def scan_restaurant(limit=100):
@@ -69,51 +120,7 @@ def print_restaurant(restaurant):
         print(review)
 
 
-def fetch_restaurant_summary(restaurant_name):
-    """
-    fetch from cache and write to cache 
-    """
-    # fetch from cache
-    start = time.perf_counter()
-    restaurant = fetch_restaurant_summary_from_cache(restaurant_name)
-    end = time.perf_counter()
-    print(f'cache query latency {(end - start) * 1000:.4f}ms')
-    # print_restaurant(restaurant)
-    # hit cache and return 
-    if restaurant:
-        print("Using cached result!")
-        return (end-start)*1000
-    # mis-cache and fetch from db
-    restaurant = fetch_restaurant_summary_from_local(restaurant_name)
-    # write to cache 
-    store_restaurant_summary_in_cache(restaurant)
-    # print response 
-    print("Using uncached result!")
-    return (end-start)*1000
 
-
-def fetch_restaurant_summary_from_db(restaurant_name):
-    """
-    from directory from db 
-    """
-    start = time.perf_counter()
-    resp = dynamodb.query(
-        TableName="Restaurants",
-        IndexName="GSI1",
-        KeyConditionExpression="GSI1PK = :gsi1pk",
-        ExpressionAttributeValues={
-            ":gsi1pk": {"S": "REST#{}".format(restaurant_name)},
-        },
-        ScanIndexForward=False,
-        Limit=6,
-    )
-    end = time.perf_counter()
-    print(f'db query latency {(end - start) * 1000: .4f}ms')
-    restaurant = Restaurant(resp["Items"][0])
-    restaurant.reviews = [Review(item) for item in resp["Items"][1:]]
-    # print_restaurant(restaurant)
-    # return 
-    return (end-start)*1000
 
 def fetch_restaurant_summary_from_local(restaurant_name):
     restaurants = []
@@ -166,7 +173,7 @@ def plot_performance(db_latencies, cache_latencies):
 # ===================================================================
 if __name__=="__main__":
     # test_connect()
-    names = scan_restaurant(limit=261)
+    names = scan_restaurant(limit=100)
     db_latencies = [fetch_restaurant_summary_from_db(name) for name in names]
     cache_latencies = [fetch_restaurant_summary(name) for name in names]
     plot_performance(db_latencies[1:], cache_latencies[1:])
