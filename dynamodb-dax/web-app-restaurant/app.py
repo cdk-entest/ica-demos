75610a014dc05bb76f96e04d4b2412cb04a4c15b
haitran <hai@entest.io>
2022-06-21 03:45:30 +0000

    update

--- dynamodb-dax/web-app-restaurant/app.py
+++ dynamodb-dax/web-app-restaurant/app.py
@@ -3,7 +3,9 @@
 
 from flask import Flask
 from flask import Flask, render_template
-from redis_client import *
+from dax_client import *
+import redis_client
+
 
 app = Flask(__name__)
 
@@ -13,13 +15,19 @@ def index():
 
 @app.route("/query-ddb")
 def query_dax():
-    restaurants = fetch_multiple_restaurants(mode='ddb', limit=100)
-    return render_template('query_ddb.html', restaurants=restaurants)
+    restaurants = fetch_multiple_restaurants(mode='ddb', limit=120)
+    return render_template('query_ddb.html', restaurants=restaurants[10:])
 
 @app.route("/query-dax")
 def query_ddb():
-    restaurants = fetch_multiple_restaurants(mode='cache', limit=100)
-    return render_template('query_cache.html', restaurants=restaurants)
+    restaurants = fetch_multiple_restaurants(mode='dax', limit=120)
+    return render_template('query_dax.html', restaurants=restaurants[10:])
+
+
+@app.route("/query-redis")
+def query_redis():
+    restaurants = redis_client.fetch_multiple_restaurants(mode='cache', limit=120)
+    return render_template('query_redis.html', restaurants=restaurants[10:])
 
 
 if __name__=="__main__":
